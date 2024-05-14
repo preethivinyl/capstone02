3fc5d7bc7770a06a6cb52adb24efd4675bf65b60
preethivinyl <74344787+preethivinyl@users.noreply.github.com>
2024-05-14 16:06:12 +0530

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,33 +1,14 @@
 #!/bin/bash
-# Check the Git branch
-if [[ $GIT_BRANCH == "origin/dev" ]]; then
-    # Build your project
-    sh'chmod +x build.sh'
-    sh'./build.sh'
+#if [[ $GIT_BRANCH == "dev" ]];then
 
-    # Log in to Docker Hub (replace with your actual Docker Hub credentials)
-    docker login -u preethivinil -p dckr_pat_Eoo1sRzg6OWiGjxvnSm3qAABSVE
-   
-  
-    # Tag the image
-    docker tag devops-build preethivinil/dev:project
+docker login -u preethivinil -p dckr_pat_Eoo1sRzg6OWiGjxvnSm3qAABSVE
+docker tag devops-build preethivinil/dev:project01
+docker push preethivinil/dev:project01
 
+#elif [[ $GIT_BRANCH == "dev" ]];then
 
-    # Push the image to the Dev Docker Hub repository
-    docker push preethivinil/dev:project
+docker login -u preethivinil -p dckr_pat_Eoo1sRzg6OWiGjxvnSm3qAABSVE
+docker tag devops-build preethivinil/prod:project01
+docker push preethivinil/prod:project01
 
-elif [[ $GIT_BRANCH == "origin/main" ]]; then
-    # Build your project
-    sh'chmod +x build.sh'
-    sh'./build.sh'
-
-    # Log in to Docker Hub (replace with your actual Docker Hub credentials)
-    docker login -u preethivinil -p dckr_pat_Eoo1sRzg6OWiGjxvnSm3qAABSVE
-
-   
-    # Tag the image
-    docker tag devops-build preethivinil/prod:project01
-
-    # Push the image to the Prod Docker Hub repository
-    docker push preethivinil/prod:project01
-fi
+#fi
